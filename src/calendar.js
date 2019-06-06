7121e1d6e46daea427c9d434d2fd4c3363ad9292
Nicholas McKenna <nicholas.a.mckenna@gmail.com>
2019-06-06 09:00:03 +0100

    Dates are selectable on calendar, used promises with timeout for feed retriving

--- src/calendar.js
+++ src/calendar.js
@@ -1,27 +1,33 @@
-console.log("fetching data from feed again");
-let eventDataForThisMonth;
+//TODO: CORS is messing with this when you don't use the callback, checek on live server
+
+let events;
 let eventDates = [];
+const showId = '8a4e7b03-893e-e911-80e8-00505601004c';
+const url = 'https://bookings.goape.co.uk/BatterseaPark/feed';
 
-getJsonFeed("mapEventsData");
+getJsonFeed("test", "2019-06-03", "2019-06-30");
 
-function getJsonFeed(callback) {
-    jQuery.ajax(
-        {
+function getJsonFeed(callback, fromDate, toDate) {
+    // TODO: Activate please wait
+    Promise.resolve(
+        jQuery.ajax({
             type: "GET",
-            url: "https://bookings.goape.co.uk/BatterseaPark/feed/eventsavailability?json&callback=" + callback + "&showid=8a4e7b03-893e-e911-80e8-00505601004c&fromdate=2019-06-03&todate=2019-06-30",
+            url: url + "/events?json&full&callback=" + callback + "&showid=" + showId + "&fromdate=" + fromDate + "&todate=" + toDate,
             dataType: "jsonp",
             jsonp: false,
             jsonpCallback: callback,
-            success: function (response) {
-                console.log(response); // server response
-                mapEventDates(response);
-            }
-        }
-    );
+            timeout: 10000
+        })
+    ).then(function (response) {
+        console.log(response); // server response
+        mapEventDates(response);
+    }).catch(function (e) {
+        console.log("error geting feed: " + e.statusText);
+    });
 }
 
 function mapEventDates(callback) {
-    let events = callback.feed.data.Events.Event;
+    events = callback.feed.Events.Event;
     let tempEventDates = []
     for (let i = 0; i < events.length; i++) {
         tempEventDates.push((new Date(events[i].ActualEventDate)).setHours(0, 0, 0, 0).valueOf());
@@ -34,7 +40,7 @@ function setupDatePicker() {
     $.datepicker.setDefaults($.datepicker.regional['']);
     $('.date_picker').datepicker({
         "beforeShowDay": beforeShowDay,
-        //"onSelect": onSelect,
+        "onSelect": onSelect,
         //"onChangeMonthYear": onChangeMonthYear,
         "dateFormat": "yy-mm-dd",
         dayNamesMin: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
@@ -44,4 +50,45 @@ function setupDatePicker() {
 
 function beforeShowDay(date) {
     return [$.inArray(date.valueOf(), eventDates) + 1, "has-events"];
+}
+
+function onSelect(date) {
+    console.log("dates selected");
+    console.log(date);
+    let legend = false;
+    let fragment = document.createDocumentFragment();
+    let eventTimesContainer = document.getElementsByClassName('event-times')[0];
+
+    if (!legend) {
+        $('.time-legend').show();
+        legend = true;
+    }
+
+    while (eventTimesContainer.firstChild) {
+        eventTimesContainer.removeChild(eventTimesContainer.firstChild);
+    }
+
+    getEventsAvailability("callbackx", date, date);
+}
+
+function getEventsAvailability(callback, fromDate, toDate) {
+    Promise.resolve(
+        jQuery.ajax({
+            type: "GET",
+            url: url + "/eventsavailability?json&callback=" + callback + "&showid=" + showId + "&fromdate=" + fromDate + "&todate=" + toDate,
+            dataType: "jsonp",
+            jsonp: false,
+            jsonpCallback: callback,
+            timeout: 10000
+        })
+    ).then(function (response) {
+        console.log(response); // server response
+    }).catch(function (e) {
+        console.log("error geting feed: " + e.statusText);
+    });
+}
+
+// Useless method
+function callbackx(result) {
+    console.log("callbackx" + result);
 }
