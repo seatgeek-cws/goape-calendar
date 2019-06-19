c09679f09f00c10982cfbc66fccee6f6df5756f6
Nicholas McKenna <nicholas.a.mckenna@gmail.com>
2019-06-19 15:20:12 +0100

    add ability to cache forward months

--- src/calendar.js
+++ src/calendar.js
@@ -1,56 +1,59 @@
 //TODO: CORS is messing with this when you don't use the callback, checek on live server
 let events;
 let eventDates = [];
-let initialLoad = true;
 let cache = [];
+let initialLoad = true;
 
 // populate values from URL
 const showId = '8a4e7b03-893e-e911-80e8-00505601004c';
 const url = 'https://bookings.goape.co.uk/BatterseaPark/';
 
+//TODO: Create start date of first event
 getJsonFeed("callbackx", "2019-06-01", "2019-06-30");
-
-// Remove Callback references when on server
+// TODO: Remove Callback references when on server
 function getJsonFeed(callback, fromDate, toDate) {
-    console.log("getJsonFeed, fromDate: " + fromDate + " toDate: " + toDate);
-    let date = [new Date(fromDate).getMonth(), new Date(fromDate).getFullYear()];
-    console.log(date);
+    let month = new Date(fromDate).getMonth();
+    let year = new Date(fromDate).getFullYear();
     // TODO: Activate please wait
-    console.log($.inArray(date[0], cache) === -1);
-    if ($.inArray(date[0], cache) === -1) {
-        Promise.resolve(
-            jQuery.ajax({
-                type: "GET",
-                cache: true,
-                url: url + "feed/events?json&callback=" + callback + "&showid=" + showId + "&fromdate=" + fromDate + "&todate=" + toDate + "&compact&disconnect=true",
-                dataType: "jsonp",
-                jsonp: false,
-                jsonpCallback: callback,
-                timeout: 10000
-            })
-        ).then(function (response) {
-            cache.push(date[0]);
-            console.log(cache);
-            mapEventDates(response);
-        }).catch(function (e) {
-            console.log("error geting feed: " + e.statusText, e);
-        });
-    }
+    Promise.resolve(
+        jQuery.ajax({
+            type: "GET",
+            cache: true,
+            url: url + "feed/events?json&callback=" + callback + "&showid=" + showId + "&fromdate=" + fromDate + "&todate=" + toDate + "&compact&disconnect=true",
+            dataType: "jsonp",
+            jsonp: false,
+            jsonpCallback: callback,
+            timeout: 10000
+        })
+    ).then(function (response) {
+        console.log('promise resolved');
+        //TODO: 
+        if (($.inArray(month, cache) === -1) && (month + 2 < 14)) {
+            cache.push(month);
+            let lastDay = lastDayOfMonth(year, month + 2);
+            let newFrom = year + '-' + (month + 2) + '-' + '01';
+            let newTo = year + '-' + (month + 2) +'-' + lastDay;
+            mapEventDates(response, month);
+            getJsonFeed("callbackx", newFrom, newTo);
+        }
+    }).catch(function (e) {
+        console.log("error geting feed: " + e.statusText, e);
+    });
 }
 
-function lazyLoadNextMonth(month) {
-    // Run in background, dont' make a new request if user selects this month
-    
-}
-
-function mapEventDates(callback) {
-    console.log("mapEventDates", callback);
+function mapEventDates(callback, month) {
     events = callback.feed.Events.Event;
     let tempEventDates = [];
     for (let i = 0; i < events.length; i++) {
         tempEventDates.push((new Date(events[i].ActualEventDate)).setHours(0, 0, 0, 0).valueOf());
     }
-    eventDates = [...new Set(tempEventDates)];
+    
+    if (eventDates.length > 0) {
+        let mergedSet = [...new Set([...eventDates, ...tempEventDates])];
+        eventDates = mergedSet;
+    } else {
+        eventDates = [...new Set(tempEventDates)];
+    }
     initialLoad ? setupDatePicker() : $('.date_picker').datepicker("refresh");
 }
 
@@ -88,14 +91,13 @@ function getEventsAvailability(callback, fromDate, toDate) {
         })
     ).then(function (response) {
         buildTimeSlotsUI(response); // server response
-        console.log(response);
     }).catch(function (e) {
         // TODO: Handle feed errors and cancel requests of timeouts and canceled sessions
         console.log('error geting feed: ' + e + " status: " + e.statusText);
     });
 }
 
-function buildTimeSlotsUI(eventFeed) {   
+function buildTimeSlotsUI(eventFeed) {
     let legend = false;
     let fragment = document.createDocumentFragment();
     let eventTimesContainer = document.getElementsByClassName('event-times')[0];
@@ -121,12 +123,11 @@ function buildTimeSlotsUI(eventFeed) {
 }
 
 function onChangeMonthYear(year, month, inst) {
-    console.log("onchangeMonthYear: " + year + month);
     let lastDay = lastDayOfMonth(year, month);
-    
+
     let fromDate = year + '-' + month + '-' + '01';
     let toDate = year + '-' + month + '-' + lastDay;
-    getJsonFeed("test", fromDate, toDate);
+    //getJsonFeed("test", fromDate, toDate);
 }
 
 function lastDayOfMonth(year, month) {
